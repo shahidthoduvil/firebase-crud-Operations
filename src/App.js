8c8954ad6273698fe624d1e11e6671629e84e04b
Shahid <shahidthoduvil.07@gmail.com>
2023-06-11 21:29:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Firebase } from './firebase/config'
+import { getDocs, collection, getFirestore, doc, setDoc, getDoc, deleteDoc, updateDoc } from 'firebase/firestore';
+import { getAuth, createUserWithEmailAndPassword, signOut, signInWithEmailAndPassword } from "firebase/auth";
+const db = getFirestore(Firebase)
 
 function App() {
+
+
+  var email = 'amal@gmail.com'
+  var password = '123456'
+
+  //signup
+  const signup = () => {
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log('user is created', userCredential.user)
+        alert('user is created')
+        // ...
+      }).catch((error) => {
+        alert(error)
+      });
+  }
+  //signin
+
+  const signin = () => {
+
+
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+      .then(Response => {
+        console.log('user is signed');
+        alert('user is signed')
+      }).catch((error) => {
+        alert(error)
+      });
+  }
+ //signout
+
+
+  const signout = () => {
+    const auth = getAuth();
+    signOut(auth).then(() => {
+
+      console.log('signout is successfull');
+      alert('signout is successfull')
+    }).catch((error) => {
+      alert(error)
+    });
+  }
+
+
+
+
+  // get all documents in a collection
+  const Showcollection = () => {
+
+    getDocs(collection(db, 'products')).then(snapshot => {
+      snapshot.forEach((obj) => {
+        console.log(obj.data(), obj.id)
+        alert('collection is got')
+      })
+    })
+  }
+
+
+  // add a document in a collection
+
+
+  const AddDocument = () => {
+
+    const productsRef = collection(db, "products");
+    setDoc(doc(productsRef), { name: 'mi', price: 3000, type: 'watch' });
+
+  }
+
+  // delte a document in a collection
+  const deletedocument = () => {
+
+    deleteDoc(doc(db, 'products', 'xWrbVpoi7GVQD5NzSw9P')).catch(err => alert(err))
+    console.log('delete')
+
+  }
+
+
+
+  const updatedocument = () => {
+    const taskref = doc(db, 'products', 'xYeVgSoiOZkgjGDU9yxl');
+    updateDoc(taskref, { name: 'hihih' });
+    console.log('update')
+
+
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={signup}>signup</button>
+      <button onClick={signin}>signin</button>
+      <button onClick={signout}>signout</button>
+      <button onClick={Showcollection}>get all</button>
+      <button onClick={AddDocument}>set document</button>
+      <button onClick={deletedocument}>delete the document</button>
+      <button onClick={updatedocument}>update the document</button>
     </div>
   );
 }
---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+import { initializeApp } from 'firebase/app'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCwXOG9sD2ab3RUF-CtaTa3kZvMwmPtVGI",
+    authDomain: "fir-472cb.firebaseapp.com",
+    projectId: "fir-472cb",
+    storageBucket: "fir-472cb.appspot.com",
+    messagingSenderId: "720228701089",
+    appId: "1:720228701089:web:a949074d2147c64966b1e0",
+    measurementId: "G-CEBV1MYFX3"
+};
+
+export const Firebase = initializeApp(firebaseConfig)
+
+
